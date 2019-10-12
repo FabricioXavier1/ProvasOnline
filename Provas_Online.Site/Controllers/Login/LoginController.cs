335b525e804772998ffe4b9dcbf6f4a1548e5643
Renan <renancossenzo@hotmail.com>
2019-10-12 16:24:35 -0300

    Criando login.

---
+++ Provas_Online.Site/Controllers/Login/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Provas_Online.Site.Models;
+
+namespace Provas_Online.Site.Controllers.Login
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(LoginModel login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Provas_Online.Site/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Provas_Online.Site.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Campo e-mail obrigatório")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Campo senha obrigatório")]
+        public string Senha { get; set; }
+    }
+}
